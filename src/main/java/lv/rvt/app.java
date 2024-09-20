b609a1130a7828b391eed2e6ac8cd85d65983f2f
23DP3EKark <166131545+23DP3EKark@users.noreply.github.com>
2024-09-20 06:35:51 +0000

    Grades and points

--- src/main/java/lv/rvt/app.java
+++ src/main/java/lv/rvt/app.java
@@ -16,12 +16,48 @@ public class app
         // System.out.println(ievade1 + " * " + ievade2 + " = " + (ievade1 * ievade2));
         // System.out.println(ievade1 + " / " + ievade2 + " = " + (1.0 * ievade1 / ievade2));
         
-        System.out.println("Give speed: ");
-        int ievade1 = Integer.valueOf(scanner.nextLine());
+        // System.out.println("Give speed: ");
+        // int ievade1 = Integer.valueOf(scanner.nextLine());
 
-        if (ievade1>120) {
-            System.out.println("Speeding ticket!");
-        }
+        // if (ievade1>120) {
+        //     System.out.println("Speeding ticket!");
+        // }
+
+            System.out.println("Give points [0-100]: ");
+            int ievade1 = Integer.valueOf(scanner.nextLine());
+
+            if (ievade1<0){
+                System.out.println("impossible!");
+            }
+
+            else if (ievade1 > 0 && ievade1<50){
+                System.out.println("failed");
+            }
+
+            else if (ievade1>49 && ievade1<60){
+                System.out.println("1");
+            }
+
+            else if (ievade1>59 && ievade1<70){
+                System.out.println("2");
+            }
+
+            else if (ievade1>69 && ievade1<80){
+                System.out.println("3");
+            }
+
+            else if (ievade1>79 && ievade1<90){
+                System.out.println("4");
+            }
+
+            else if (ievade1>89 && ievade1<=100){
+                System.out.println("5");
+            }
+
+            else{
+                System.out.println("incredible!");
+            }
+        
     }
 
 }
