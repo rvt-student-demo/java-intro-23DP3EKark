b98e0681feb6dce9973996bfd41fb92841ec07d4
23DP3EKark <166131545+23DP3EKark@users.noreply.github.com>
2024-09-20 05:46:28 +0000

    Speeding ticket exercise

--- src/main/java/lv/rvt/app.java
+++ src/main/java/lv/rvt/app.java
@@ -5,16 +5,23 @@ public class app
 {
     public static void main( String[] args ){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Give the first number:");
+        // System.out.println("Give the first number:");
+        // int ievade1 = Integer.valueOf(scanner.nextLine());
+
+        // System.out.println("Give the first number:");
+        // int ievade2 = Integer.valueOf(scanner.nextLine());
+
+        // System.out.println(ievade1 + " + " + ievade2 + " = " + (ievade1 + ievade2));
+        // System.out.println(ievade1 + " - " + ievade2 + " = " + (ievade1 - ievade2));
+        // System.out.println(ievade1 + " * " + ievade2 + " = " + (ievade1 * ievade2));
+        // System.out.println(ievade1 + " / " + ievade2 + " = " + (1.0 * ievade1 / ievade2));
+        
+        System.out.println("Give speed: ");
         int ievade1 = Integer.valueOf(scanner.nextLine());
 
-        System.out.println("Give the first number:");
-        int ievade2 = Integer.valueOf(scanner.nextLine());
-
-        System.out.println(ievade1 + " + " + ievade2 + " = " + (ievade1 + ievade2));
-        System.out.println(ievade1 + " - " + ievade2 + " = " + (ievade1 - ievade2));
-        System.out.println(ievade1 + " * " + ievade2 + " = " + (ievade1 * ievade2));
-        System.out.println(ievade1 + " / " + ievade2 + " = " + (1.0 * ievade1 / ievade2));
+        if (ievade1>120) {
+            System.out.println("Speeding ticket!");
+        }
     }
 
 }
