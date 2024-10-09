6b24509c6ceea7ea32444c9c71c9604f9125aba2
23DP3EKark <166131545+23DP3EKark@users.noreply.github.com>
2024-10-09 06:47:36 +0000

    Divisible by three

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,179 +5,21 @@ public class App
 {
     public static void main( String[] args ){
         Scanner scanner = new Scanner(System.in);
-        // System.out.println("Give the first number:");
-        // int ievade1 = Integer.valueOf(scanner.nextLine());
-
-        // System.out.println("Give the first number:");
-        // int ievade2 = Integer.valueOf(scanner.nextLine());
-
-        // System.out.println(ievade1 + " + " + ievade2 + " = " + (ievade1 + ievade2));
-        // System.out.println(ievade1 + " - " + ievade2 + " = " + (ievade1 - ievade2));
-        // System.out.println(ievade1 + " * " + ievade2 + " = " + (ievade1 * ievade2));
-        // System.out.println(ievade1 + " / " + ievade2 + " = " + (1.0 * ievade1 / ievade2));
         
-        // System.out.println("Give speed: ");
-        // int ievade1 = Integer.valueOf(scanner.nextLine());
+        System.out.println("no");
+        int no = Integer.valueOf(scanner.nextLine());
+        System.out.println("lidz");
+        int lidz = Integer.valueOf(scanner.nextLine());
 
-        // if (ievade1>120) {
-        //     System.out.println("Speeding ticket!");
-        // }
-
-            // System.out.println("Give points [0-100]: ");
-            // int ievade1 = Integer.valueOf(scanner.nextLine());
-
-            // if (ievade1<0){
-            //     System.out.println("impossible!");
-            // }
-
-            // else if (ievade1 > 0 && ievade1<50){
-            //     System.out.println("failed");
-            // }
-
-            // else if (ievade1>49 && ievade1<60){
-            //     System.out.println("1");
-            // }
-
-            // else if (ievade1>59 && ievade1<70){
-            //     System.out.println("2");
-            // }
-
-            // else if (ievade1>69 && ievade1<80){
-            //     System.out.println("3");
-            // }
-
-            // else if (ievade1>79 && ievade1<90){
-            //     System.out.println("4");
-            // }
-
-            // else if (ievade1>89 && ievade1<=100){
-            //     System.out.println("5");
-            // }
-
-            // else{
-            //     System.out.println("incredible!");
-            // }
-
-            // System.out.println("Value of the gift?");
-
-            // int nodoklis = Integer.valueOf(scanner.nextLine());
-
-            // if (nodoklis < 5000) {
-            //     System.out.println("No tax!");
-            // }
-
-            // else if (nodoklis >= 5000 && nodoklis < 25000) {
-            //     System.out.println("Tax: " + (100 + (nodoklis-5000) * 0.08));
-            // }
-
-            // else if (nodoklis >= 25000 && nodoklis < 55000) {
-            //     System.out.println("Tax: " + (1700 + (nodoklis-25000) * 0.1));
-            // }
-
-            // else if (nodoklis >= 55000 && nodoklis < 200000) {
-            //     System.out.println("Tax: " + (4700 + (nodoklis-55000) * 0.12));
-            // }
-
-            // else if (nodoklis >= 200000 && nodoklis < 1000000) {
-            //     System.out.println("Tax: " + (22100 + (nodoklis-200000) * 0.15));
-            // }
-
-            // else if (nodoklis >= 1000000) {
-            //     System.out.println("Tax: " + (142100 + (nodoklis-1000000) * 0.17));
-            // }
-
-            // int summa = 0;
-            // int daudzums = 0;
-
-
-            // while (true) {
-            //     System.out.println("Give a number: ");
-            //      int ievade = Integer.valueOf(scanner.nextLine());
-
-            //      int sum = 0;
-
-            //     if (ievade == 0) {
-            //         break;
-
-            //     }
-            //     else {
-            //         daudzums += 1;
-            //         summa = summa + ievade;
-            //     }
-
-            // }
-            // System.out.println("Ammount of numbers: " + daudzums);
-            // System.out.println("Sum of numbers: " + summa);
-
-
-            // int skaitlis = Integer.valueOf(scanner.nextLine());
-
-            //  for (int i = 0; i <= skaitlis; i++) {
-            //     System.out.println(i);
-            //  }
-
-            // int summa = 0;
-
-            // System.out.println("Last number? ");
-            // int ievade = Integer.valueOf(scanner.nextLine());
-
-            // for (int i = 0; i <= ievade; i++) {
-            //     summa += i;
-            // }
-
-            // System.out.println("The sum is " + summa);
-
-
-
-            // for (int x = 10; x < 100; x += 10){
-            //     System.out.println(x);
-            // }
-
-
-
-            // System.out.println("Give numbers: ");
-            
-            // int daudzums = 0;
-            // int summa = 0;
-            // int odd = 0;
-            // int even = 0;
-
-            // while (true) {
-            //     int ievade = Integer.valueOf(scanner.nextLine());
-
-            //     if (ievade == -1) {
-            //         break;
-            //     }
-            //     else if (ievade % 2 == 0) {
-            //         summa = summa + ievade;
-            //         daudzums += 1;
-            //         even += 1;
-            //     }
-            //     else {
-            //         summa = summa + ievade;
-            //         daudzums += 1;
-            //         odd += 1;
-            //     }
-            //     }
-            //     System.out.println("Thx! Bye!");
-            //     System.out.println("Sum: " + summa);
-            //     System.out.println("Numbers: " + daudzums);
-            //     System.out.println("Average: " + (1.0 * summa / daudzums));
-            //     System.out.println("Even: " + even);
-            //     System.out.println("Odd: " + odd);
-
-            System.out.println("Cik reizes vēlaties tikt sasveicināts?");
-            int ievade = Integer.valueOf(scanner.nextLine());
-
-            for (int reizes = 0; reizes < ievade; reizes++) {
-                greet();
-            }
+        divisibleByThreeInRange(no, lidz);
 
     }
     
-    public static void greet() {
-        System.out.println("Sveiki!");
-
-    
+    public static void divisibleByThreeInRange(int no, int lidz) {
+        for (int skaitlis = no; skaitlis <= lidz; skaitlis++){
+            if (skaitlis % 3 == 0) {
+                System.out.println(skaitlis);
+            }
+        }
     }
 }
