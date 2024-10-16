8bbd72f95c2ea8c1a047d82a9dc7aa0bbb6e122b
23DP3EKark <166131545+23DP3EKark@users.noreply.github.com>
2024-10-16 07:47:19 +0000

    Second plus third

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,7 +5,23 @@ public class App{
     public static void main( String[] args ){
         Scanner scanner = new Scanner(System.in);
 
-        Stars.reverseTriangle(5);
+        ArrayList<Integer> saraksts = new ArrayList<>();
 
+        int sum = 0;
+
+        while (true){
+
+            int ievade = Integer.valueOf(scanner.nextLine());
+
+                if (ievade == 0){
+                    System.out.println(sum);
+                    break;
+                }
+
+                else{
+                    saraksts.add(ievade);
+                    sum = sum + ievade;
+                }
+        }
     }
 }
