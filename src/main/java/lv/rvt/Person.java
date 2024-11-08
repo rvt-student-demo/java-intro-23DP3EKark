22a36baae39b5d68578d2ea160bb31242b929276
23DP3EKark <166131545+23DP3EKark@users.noreply.github.com>
2024-11-08 07:30:15 +0000

    Person

--- src/main/java/lv/rvt/Person.java
+++ src/main/java/lv/rvt/Person.java
@@ -3,4 +3,9 @@ package lv.rvt;
 public class Person {
     private String name;
     private int age;
+
+    public Person(String initialName) {
+        this.age = 0;
+        this.name = initialName;
+    }
 }
