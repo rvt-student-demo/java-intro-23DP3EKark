3794a1a671927fe8304e86fa6564de880b60345c
23DP3EKark <166131545+23DP3EKark@users.noreply.github.com>
2024-11-22 06:37:08 +0000

    Pievienoju books lai vs code vinu varetu trackot

---
+++ src/main/java/lv/rvt/Books.java
@@ -0,0 +1,16 @@
+package lv.rvt;
+
+import java.util.ArrayList;
+
+public class Books {
+    private String title;
+    private Integer pagecnt;
+    private Integer years;
+
+    ArrayList<String> titles = new ArrayList<>();
+    ArrayList<Integer> pages = new ArrayList<>();
+    ArrayList<Integer> year = new ArrayList<>();
+
+
+
+}
