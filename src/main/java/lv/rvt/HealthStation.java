cef181b46927620df5fa5ebdd34ff82260660fce
23DP3EKark <166131545+23DP3EKark@users.noreply.github.com>
2024-12-06 09:03:21 +0000

    Part 3: Counting weighings

--- src/main/java/lv/rvt/HealthStation.java
+++ src/main/java/lv/rvt/HealthStation.java
@@ -2,9 +2,16 @@ package lv.rvt;
 
 public class HealthStation {
 
+private int count;
+
 public int weigh(Person person) {
-        return person.weight;
+    this.count = this.count + 1;    
+    return person.weight;
     }
+
+public int weighings() {
+    return count;
+}
 
 public void feed(Person person) {
     person.weight = person.weight + 1;
