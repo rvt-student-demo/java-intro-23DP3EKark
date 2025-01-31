c8e58c283ce5e236c56986a80e45ef9a64bb802a
23DP3EKark <166131545+23DP3EKark@users.noreply.github.com>
2025-01-31 07:47:00 +0000

    Part 2 Student

--- src/main/java/lv/rvt/Main.java
+++ src/main/java/lv/rvt/Main.java
@@ -7,10 +7,19 @@ import javax.swing.Box;
 
 public class Main {
     public static void main(String[] args) {
-        Person ada = new Person("Ada Lovelace", "24 Maddox St. London W1S 2QN");
-        Person esko = new Person("Esko Ukkonen", "Mannerheimintie 15 00100 Helsinki");
+        // Person ada = new Person("Ada Lovelace", "24 Maddox St. London W1S 2QN");
+        // Person esko = new Person("Esko Ukkonen", "Mannerheimintie 15 00100 Helsinki");
+
+        // System.out.println(ada);
+        // System.out.println(esko);
         
-        System.out.println(ada);
-        System.out.println(esko);
+        Student ollie = new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028");
+
+        System.out.println(ollie);
+        System.out.println("Study credits " + ollie.credits());
+
+        ollie.study();
+
+        System.out.println("Study credits "+ ollie.credits());
     }
 }
--- src/main/java/lv/rvt/Student.java
+++ src/main/java/lv/rvt/Student.java
@@ -1,5 +1,18 @@
 package lv.rvt;
 
-public class Student {
+public class Student extends Person {
 
+    public int credits;
+
+    public Student(String name, String street){
+        super(name, street);
+    }
+
+    public int study() {
+        return this.credits += 1;
+    }
+
+    public int credits() {
+        return this.credits;
+    }
 }
