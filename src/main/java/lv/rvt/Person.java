162f30f82e463e7573d4b4267748bf05892f3c69
23DP3EKark <166131545+23DP3EKark@users.noreply.github.com>
2025-02-21 07:30:26 +0000

    personmanager part 2 un 3 vel nestrada, nav pabeigts

--- src/main/java/lv/rvt/Person.java
+++ src/main/java/lv/rvt/Person.java
@@ -1,19 +1,28 @@
 package lv.rvt;
 
 public class Person {
-    public String name;
-    public String street;
+    private String name;
+    private int age;
+    private double weight;
+    private double height;
+    private String address;
 
-
-    public Person(String name, String street) {
+    public Person(String name, int age, double weight, double height, String address) {
         this.name = name;
-        this.street = street;
+        this.age = age;
+        this.weight = weight;
+        this.height = height;
+        this.address = address;
     }
+
+    public String getName() { return name; }
+    public int getAge() { return age; }
+    public double getWeight() { return weight; }
+    public double getHeight() { return height; }
+    public String getAddress() { return address; }
 
     @Override
-    public String toString(){
-        return this.name + '\n' + ' ' + this.street;
+    public String toString() {
+        return name + "," + age + "," + weight + "," + height + "," + address;
     }
-
-
 }
