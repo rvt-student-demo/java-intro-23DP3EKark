2c52b263613426e5ff13b4ac01113be01dd9f920
23DP3EKark <166131545+23DP3EKark@users.noreply.github.com>
2025-02-27 18:28:50 +0000

    Money part 1, 2

--- src/main/java/lv/rvt/Main.java
+++ src/main/java/lv/rvt/Main.java
@@ -6,53 +6,13 @@ import java.io.FileWriter;
 
 public class Main {
     public static void main(String[] args) {
-        
-    }
-
-    public static ArrayList<Person> getPersonList() throws Exception {
-        BufferedReader reader = Helper.getReader("persons.csv");
-
-        ArrayList<Person> personList = new ArrayList<>();
-        String line;
-
-        reader.readLine();
-        while ((line = reader.readLine()) != null) {
-            String[] parts = line.split(", ");
-
-            if (parts.length < 5) continue;
-            
-            String name = parts[0];
-            int age = Integer.parseInt(parts[1]);
-            int weight = Integer.parseInt(parts[2]);
-            int height = Integer.parseInt(parts[3]);
-            String address = parts[4];
-
-            Person person = new Person(name, age, weight, height, address);
-            personList.add(person);
-        }
-
-        reader.close();
-        return personList;
-    }
-
-    public static void addPerson(Person person) throws Exception {
-        String data = person.toString();
-
-        try (BufferedWriter buffer = new BufferedWriter(new FileWriter("persons.csv", true))) { 
-            buffer.write(data);
-            buffer.newLine();
-            buffer.flush();
-            System.out.println("Person has been written to the file!");
-        } 
-    }
-
-    public static void printPersonTable(ArrayList<Person> personList) {
-        System.out.printf("%-15s %-5s %-7s %-7s %-20s%n", "Name", "Age", "Weight", "Height", "Address");
-        System.out.println("----------------------------------------------------------");
     
-        for (Person person : personList) {
-            System.out.printf("%-15s %-5d %-7d %-7d %-20s%n",
-                    person.getName(), person.getAge(), person.getWeight(), person.getHeight(), person.getAddress());
-        }
+        Money a = new Money(10, 0);
+        Money b = new Money(3, 0);
+        Money c = new Money(5, 0);
+
+        System.out.println(a.lessThan(b));  // false
+        System.out.println(b.lessThan(c));  // true
+
     }
 }
---
+++ src/main/java/lv/rvt/Money.java
@@ -0,0 +1,43 @@
+package lv.rvt;
+
+public class Money {
+    private final int euros;
+    private final int cents;
+
+    public Money(int euros, int cents) {
+        this.euros = euros;
+        this.cents = cents;
+    }
+
+    public int euros() {
+        return euros;
+    }
+
+    public int cents() {
+        return cents;
+    }
+
+    public String toString() {
+        String zero = "";
+        if (cents <= 10) {
+            zero = "0";
+        }
+
+        return euros + "." + zero + cents + "e";
+    }
+
+    public Money plus(Money addition) {
+        int totalCents = (this.euros * 100 + this.cents) + (addition.euros * 100 + addition.cents);
+        return new Money(totalCents / 100, totalCents % 100);
+    }
+
+    public boolean lessThan(Money compared) {
+        if (this.euros * 100 + this.cents < compared.euros * 100 + compared.cents ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+}
